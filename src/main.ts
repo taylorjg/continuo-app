b483c0a84f2f28a17fe769afa4d6930f3fbb68ee
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-07-05 22:08:53 +0100

    Place 2 cards initially and then place further cards when Next Card is clicked

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { Board } from './continuo-lib/board'
 import { Card } from './continuo-lib/card'
 import { Deck } from './continuo-lib/deck'
 import { Colour, Orientation } from './continuo-lib/enums'
-import { evaluatePlacedCard } from './continuo-lib/evaluate'
+import { evaluateCard } from './continuo-lib/evaluate'
 import { PlacedCard } from './continuo-lib/placedCard'
 
 const CELL_SIZE = 28 * 2
@@ -46,12 +46,14 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 export class GameScene extends Phaser.Scene {
 
   deck: Deck
+  board: Board
   normalCardSpritesMap: Map<Card, Phaser.GameObjects.Sprite>
   rotatedCardSpritesMap: Map<Card, Phaser.GameObjects.Sprite>
 
   constructor() {
     super(sceneConfig)
     this.deck = new Deck()
+    this.board = Board.empty
   }
 
   private static getCardPosition(row: number, col: number): Phaser.Geom.Point {
@@ -64,6 +66,17 @@ export class GameScene extends Phaser.Scene {
     const x = col * QUARTER_CARD_SIZE + HALF_CELL_SIZE + ARBITRARY_OFFSET_X
     const y = row * QUARTER_CARD_SIZE + HALF_CELL_SIZE + ARBITRARY_OFFSET_Y
     return new Phaser.Geom.Point(x, y)
+  }
+
+  private placeCard(placedCard: PlacedCard): void {
+    this.board = this.board.placeCard(placedCard)
+    const map = placedCard.orientation == Orientation.North || placedCard.orientation == Orientation.South
+      ? this.normalCardSpritesMap
+      : this.rotatedCardSpritesMap
+    const cardSprite = map.get(placedCard.card)
+    const pos = GameScene.getCardPosition(placedCard.row, placedCard.col)
+    cardSprite.setPosition(pos.x, pos.y)
+    cardSprite.visible = true
   }
 
   public create() {
@@ -89,36 +102,23 @@ export class GameScene extends Phaser.Scene {
       makeCardSprite(card, index, true /* rotated */)
     })
 
-    const [card1, orientation1] = Deck.findCard(Colour.Blue, Colour.Green, Colour.Green, Colour.Blue)
-    const [card2, orientation2] = Deck.findCard(Colour.Blue, Colour.Green, Colour.Red, Colour.Blue)
-    const [card3, orientation3] = Deck.findCard(Colour.Blue, Colour.Red, Colour.Red, Colour.Yellow)
-    const [card4, orientation4] = Deck.findCard(Colour.Blue, Colour.Green, Colour.Yellow, Colour.Blue)
-    const placedCards = [
-      new PlacedCard(card1, 0, 0, orientation1),
-      new PlacedCard(card2, -1, 4, orientation2),
-      new PlacedCard(card3, 3, 5, orientation3),
-      new PlacedCard(card4, 4, 1, orientation4)
-    ]
-    const board = placedCards.slice(0, -1).reduce((b, pc) => b.placeCard(pc), Board.empty)
-    const possibleMove = evaluatePlacedCard(board, placedCards.slice(-1)[0])
+    const card1 = this.deck.nextCard()
+    const placedCard = new PlacedCard(card1, 0, 0, Orientation.North)
+    this.placeCard(placedCard)
 
-    placedCards.forEach(placedCard => {
-      const sprite = placedCard.orientation == Orientation.North || Orientation.South
-        ? this.normalCardSpritesMap.get(placedCard.card)
-        : this.rotatedCardSpritesMap.get(placedCard.card)
-      const pos = GameScene.getCardPosition(placedCard.row, placedCard.col)
-      sprite.setPosition(pos.x, pos.y)
-      sprite.visible = true
-    })
+    const card2 = this.deck.nextCard()
+    const card2BestMove = evaluateCard(this.board, card2)[0]
+    this.placeCard(card2BestMove.placedCard)
 
-    possibleMove.chains.forEach(chain => {
-      const points = chain.cells.map(cell => GameScene.getCellPosition(cell.row, cell.col))
-      const polygon = new Phaser.GameObjects.Polygon(this, 0, 0, points)
-      polygon.setStrokeStyle(CELL_SIZE / 5, 0xFF00FF)
-      polygon.closePath = false
-      polygon.setOrigin(0, 0)
-      this.add.existing(polygon)
-    })
+    // const possibleMove = evaluatePlacedCard(board, placedCards.slice(-1)[0])
+    // possibleMove.chains.forEach(chain => {
+    //   const points = chain.cells.map(cell => GameScene.getCellPosition(cell.row, cell.col))
+    //   const polygon = new Phaser.GameObjects.Polygon(this, 0, 0, points)
+    //   polygon.setStrokeStyle(CELL_SIZE / 5, 0xFF00FF)
+    //   polygon.closePath = false
+    //   polygon.setOrigin(0, 0)
+    //   this.add.existing(polygon)
+    // })
 
     const restartButton = this.add.dom(0, 0, 'button', 'margin: 10px; width: 80px;', 'Restart')
     restartButton.setOrigin(0, 0)
@@ -134,14 +134,18 @@ export class GameScene extends Phaser.Scene {
 
     restartButton.on('click', () => {
       console.log('[Restart button click]')
-      this.normalCardSpritesMap.forEach(sprite => sprite.visible = false)
-      this.rotatedCardSpritesMap.forEach(sprite => sprite.visible = false)
+      this.normalCardSpritesMap.forEach(cardSprite => cardSprite.visible = false)
+      this.rotatedCardSpritesMap.forEach(cardSprite => cardSprite.visible = false)
       this.deck.reset()
+      this.board = Board.empty
       nextCardElement.disabled = false
     }, this)
 
     nextCardButton.on('click', () => {
       console.log('[NextCard button click]')
+      const card = this.deck.nextCard()
+      const bestMove = evaluateCard(this.board, card)[0]
+      this.placeCard(bestMove.placedCard)
       nextCardElement.disabled = this.deck.numCardsLeft == 0
     }, this)
   }
