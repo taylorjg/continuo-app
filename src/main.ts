c6c93cc09f100dbd0f0ecfcfc1ed93f8b6e25864
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-07-08 23:47:22 +0100

    Move buttons into a separate HUD scene

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ const drawCard = (graphics: Phaser.GameObjects.Graphics, card: Card, orientation
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
   visible: false,
-  key: 'Game'
+  key: 'GameScene'
 }
 
 export class GameScene extends Phaser.Scene {
@@ -146,6 +146,15 @@ export class GameScene extends Phaser.Scene {
       makeCardSprite(card, index, true /* rotated */)
     })
 
+    this.startNewGame()
+  }
+
+  private startNewGame(): void {
+    this.normalCardSpritesMap.forEach(cardSprite => cardSprite.visible = false)
+    this.rotatedCardSpritesMap.forEach(cardSprite => cardSprite.visible = false)
+    this.deck.reset()
+    this.board = Board.empty
+
     const card1 = this.deck.nextCard()
     const placedCard1 = new PlacedCard(card1, 0, 0, Orientation.North)
     const move1 = new PossibleMove(placedCard1, [])
@@ -154,42 +163,59 @@ export class GameScene extends Phaser.Scene {
     const card2 = this.deck.nextCard()
     const move2 = evaluateCard(this.board, card2)[0]
     this.placeCard(move2)
+  }
+
+  public onRestart(nextCardElement: HTMLButtonElement): void {
+    this.startNewGame()
+    nextCardElement.disabled = false
+  }
+
+  public onNextCard(nextCardElement: HTMLButtonElement): void {
+    const card = this.deck.nextCard()
+    const move = evaluateCard(this.board, card)[0]
+    this.placeCard(move)
+    nextCardElement.disabled = this.deck.numCardsLeft == 0
+  }
+}
+
+export class HUDScene extends Phaser.Scene {
+
+  gameScene: GameScene
+  nextCardElement: HTMLButtonElement
+
+  constructor() {
+    super({
+      active: true,
+      visible: true,
+      key: 'HUDScene'
+    })
+  }
+
+  create() {
+    this.gameScene = <GameScene>this.scene.get('GameScene')
 
     const restartButton = this.add.dom(0, 0, 'button', 'margin: 10px; width: 80px;', 'Restart')
     restartButton.setOrigin(0, 0)
     restartButton.addListener('click')
 
-    const nextCardElement = document.createElement('button')
-    nextCardElement.style.margin = '10px'
-    nextCardElement.style.width = '80px'
-    nextCardElement.innerText = 'Next Card'
-    const nextCardButton = this.add.dom(0, 30, nextCardElement)
+    this.nextCardElement = document.createElement('button')
+    this.nextCardElement.style.margin = '10px'
+    this.nextCardElement.style.width = '80px'
+    this.nextCardElement.innerText = 'Next Card'
+    const nextCardButton = this.add.dom(0, 30, this.nextCardElement)
     nextCardButton.setOrigin(0, 0)
     nextCardButton.addListener('click')
 
-    restartButton.on('click', () => {
-      console.log('[Restart button click]')
-      this.normalCardSpritesMap.forEach(cardSprite => cardSprite.visible = false)
-      this.rotatedCardSpritesMap.forEach(cardSprite => cardSprite.visible = false)
-      this.deck.reset()
-      this.board = Board.empty
-      const card1 = this.deck.nextCard()
-      const placedCard1 = new PlacedCard(card1, 0, 0, Orientation.North)
-      const move1 = new PossibleMove(placedCard1, [])
-      this.placeCard(move1)
-      const card2 = this.deck.nextCard()
-      const move2 = evaluateCard(this.board, card2)[0]
-      this.placeCard(move2)
-      nextCardElement.disabled = false
-    }, this)
+    restartButton.on('click', this.onRestart, this)
+    nextCardButton.on('click', this.onNextCard, this)
+  }
 
-    nextCardButton.on('click', () => {
-      console.log('[NextCard button click]')
-      const card = this.deck.nextCard()
-      const move = evaluateCard(this.board, card)[0]
-      this.placeCard(move)
-      nextCardElement.disabled = this.deck.numCardsLeft == 0
-    }, this)
+  public onRestart(): void {
+    this.gameScene.onRestart(this.nextCardElement)
+  }
+
+  public onNextCard(): void {
+    this.gameScene.onNextCard(this.nextCardElement)
   }
 }
 
@@ -201,7 +227,7 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
     height: window.innerHeight
   },
   backgroundColor: '#AAAAAA',
-  scene: GameScene,
+  scene: [GameScene, HUDScene],
   parent: 'game',
   dom: {
     createContainer: true
